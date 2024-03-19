2589a18a64f00ce8f28de8a915ea3856f24ab8c4
monika-encanto <monika.singh@encantotek.in>
2024-03-19 11:06:01 +0530

    Update the databse configuration in the settings.py file.

--- EduSmart/settings.py
+++ EduSmart/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-b^&%diqg(h(@4rs^9bhm*9horj^!gkt8)2a@ze%q10a5xwzq6j
 DEBUG = True
 
 ALLOWED_HOSTS = ['edu-management.azurewebsites.net']
-
 
 # Application definition
 
@@ -42,7 +40,7 @@ INSTALLED_APPS = [
     # external libraries
     'rest_framework',
     'rest_framework_simplejwt',
-    
+
     # internal apps
     'authentication',
     'student',
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EduSmart.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -88,16 +85,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgresadmin',
-        'PASSWORD': 'Edusmartdb98#$!',
-        'HOST':'edu-postgresqldb.postgres.database.azure.com',
-        'PORT':5432,
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        'USER': 'edusmartadmin',
+        'PASSWORD': 'Chowdary1#',
+        'HOST': 'edusmart.postgres.database.azure.com',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -117,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -130,7 +122,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
