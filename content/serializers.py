2cc7d6eccf9de259b842b9222705abf354cf97bb
deepesh-eight <deepeshsonaniya@gmail.com>
2024-05-05 17:27:55 +0530

    Updated the response for content API

--- content/serializers.py
+++ content/serializers.py
@@ -1,8 +1,17 @@
 import json
 from rest_framework import serializers
-from .models import Content
+from .models import Content, Curriculum
+
+class CurriculumData(serializers.ModelSerializer):
+    class Meta:
+        model = Curriculum
+        fields = ['curriculum_name']
 
 class ContentSerializer(serializers.ModelSerializer):
+    curriculum = serializers.SerializerMethodField()
     class Meta:
         model = Content
-        fields = '__all__'
+        fields = ['id','curriculum','content_media','content_media_link','content_type','content_name','content_creator','supporting_detail','description','is_recommended','classes','subject']
+
+    def get_curriculum(self, obj):
+        return obj.curriculum.curriculum_name if obj.curriculum else None
