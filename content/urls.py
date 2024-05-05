b75772c3ef7756e7d35304b4c32eb539727eb132
deepesh-eight <deepeshsonaniya@gmail.com>
2024-05-05 16:37:29 +0530

    Added API to get content for student

--- content/urls.py
+++ content/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ContentCreateView, ContentListView, ContentDeleteView, SuperAdminContentCreateView
+from .views import ContentCreateView, ContentListView, ContentDeleteView, SuperAdminContentCreateView, StudentContentListView
 
 urlpatterns = [
     path('super-admin/create/', SuperAdminContentCreateView.as_view(), name='super-admincontent-create'),
     path('create/', ContentCreateView.as_view(), name='content-create'),
     path('get-all-data/', ContentListView.as_view(), name='get-all-content-data'),
     path('delete/<int:pk>/', ContentDeleteView.as_view(), name='content-delete'),
+    path('get-all-student-content/', StudentContentListView.as_view(), name='get-all-student-content'),
 ]
