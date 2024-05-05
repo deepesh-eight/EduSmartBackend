19ae9d4c010590f969192023d4701301651589b9
monika-encanto <monika.singh@encantotek.in>
2024-05-05 13:42:20 +0530

    Update the student user profile API.

--- student/serializers.py
+++ student/serializers.py
@@ -272,9 +272,13 @@ class StudentUserProfileSerializer(serializers.ModelSerializer):
         return obj.user.email if hasattr(obj, 'user') else None
 
     def get_subjects(self, obj):
-        subject = Curriculum.objects.get(curriculum_name=obj.curriculum, select_class=obj.class_enrolled)
-        if subject:
-            return subject.primary_subject
+        curriculum = Curriculum.objects.get(curriculum_name=obj.curriculum, select_class=obj.class_enrolled)
+        subject_data = Subjects.objects.filter(curriculum_id=curriculum.id)
+        subject = []
+        if subject_data:
+            for subject_list in subject_data:
+                subject.append(subject_list.primary_subject)
+            return subject
         else:
             None
 
