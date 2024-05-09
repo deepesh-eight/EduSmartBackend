135d27bb062c841a0f7d68c7e3ab0c0f9e7ae923
monika-encanto <monika.singh@encantotek.in>
2024-05-09 16:15:06 +0530

    Added the category in the content list API.

--- content/serializers.py
+++ content/serializers.py
@@ -29,7 +29,7 @@ class ContentListSerializer(serializers.ModelSerializer):
     content_media = serializers.SerializerMethodField()
     class Meta:
         model = Content
-        fields = ['id','curriculum','content_media', 'image', 'content_media_link','content_type','content_name','content_creator','supporting_detail','description','is_recommended','classes','subject']
+        fields = ['id','curriculum','content_media', 'image', 'content_media_link','content_type', 'category', 'content_name','content_creator','supporting_detail','description','is_recommended','classes','subject']
 
     # def get_curriculum(self, obj):
     #     return obj.curriculum.curriculum_name if obj.curriculum else None
