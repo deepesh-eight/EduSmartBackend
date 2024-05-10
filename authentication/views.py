ee56167d0f1e64d9d2aae44b38698b8a24d633c1
monika-encanto <monika.singh@encantotek.in>
2024-05-10 18:58:55 +0530

    Update the curriculum, class, section, and subject list API.

--- authentication/views.py
+++ authentication/views.py
@@ -772,30 +772,22 @@ class TeacherCurriculumListView(APIView):
     """
     This class is created to fetch the list of classes, sections, and subject.
     """
-    permission_classes = [IsTeacherUser, IsInSameSchool]
+    permission_classes = [permissions.IsAuthenticated, IsInSameSchool]
 
     def get(self, request):
         try:
-            user = request.user
-            if user.user_type == 'teacher':
-                data = Curriculum.objects.filter(school_id=request.user.school_id).values_list('curriculum_name',flat=True).distinct()
-                curriculum = list(data)
-                curriculum_list = {
-                    "curriculum_name": curriculum
-                }
-                response_data = create_response_data(
-                    status=status.HTTP_200_OK,
-                    message=CurriculumMessage.CURRICULUM_LIST_MESSAGE,
-                    data=curriculum_list
-                )
-                return Response(response_data, status=status.HTTP_200_OK)
-            else:
-                response_data = create_response_data(
-                    status=status.HTTP_400_BAD_REQUEST,
-                    message="you are not an teacher user.",
-                    data={}
-                )
-                return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+            data = Curriculum.objects.filter(school_id=request.user.school_id).values_list('curriculum_name',flat=True).distinct()
+            curriculum = list(data)
+            curriculum_list = {
+                "curriculum_name": curriculum
+            }
+            response_data = create_response_data(
+                status=status.HTTP_200_OK,
+                message=CurriculumMessage.CURRICULUM_LIST_MESSAGE,
+                data=curriculum_list
+            )
+            return Response(response_data, status=status.HTTP_200_OK)
+
         except Exception as e:
             response_data = create_response_data(
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -809,31 +801,24 @@ class TeacherCurriculumSectionListView(APIView):
     """
     This class is created to fetch the list of sections in the provided class.
     """
-    permission_classes = [IsTeacherUser, IsInSameSchool]
+    permission_classes = [permissions.IsAuthenticated, IsInSameSchool]
 
     def get(self, request):
         try:
             user = request.user
-            if user.user_type == 'teacher':
-                curriculum = request.query_params.get("curriculum")
-                classes = request.query_params.get("class_name")
-                section_list = StudentUser.objects.filter(user__school_id=request.user.school_id, curriculum=curriculum,
-                                                          class_enrolled=classes).values_list('section', flat=True).distinct()
-                # serializer = CurriculumSectionListSerializer(section_list, many=True)
-                # class_names = [item['section'] for item in serializer.data]
-                response_data = create_response_data(
-                    status=status.HTTP_200_OK,
-                    message=CurriculumMessage.SECTION_LIST_MESSAGE,
-                    data=list(section_list)
-                )
-                return Response(response_data, status=status.HTTP_200_OK)
-            else:
-                response_data = create_response_data(
-                    status=status.HTTP_400_BAD_REQUEST,
-                    message="you are not an teacher user.",
-                    data={}
-                )
-                return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+            curriculum = request.query_params.get("curriculum")
+            classes = request.query_params.get("class_name")
+            section_list = StudentUser.objects.filter(user__school_id=request.user.school_id, curriculum=curriculum,
+                                                      class_enrolled=classes).values_list('section', flat=True).distinct()
+            # serializer = CurriculumSectionListSerializer(section_list, many=True)
+            # class_names = [item['section'] for item in serializer.data]
+            response_data = create_response_data(
+                status=status.HTTP_200_OK,
+                message=CurriculumMessage.SECTION_LIST_MESSAGE,
+                data=list(section_list)
+            )
+            return Response(response_data, status=status.HTTP_200_OK)
+
         except Exception as e:
             response_data = create_response_data(
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -847,29 +832,21 @@ class TeacherCurriculumCListView(APIView):
     """
     This class is created to fetch the list of classes in the provided curriculum.
     """
-    permission_classes = [IsTeacherUser, IsInSameSchool]
+    permission_classes = [permissions.IsAuthenticated, IsInSameSchool]
 
     def get(self, request):
         try:
-            user = request.user
-            if user.user_type == 'teacher':
-                curriculum = request.query_params.get("curriculum")
-                data = Curriculum.objects.filter(curriculum_name=curriculum, school_id=request.user.school_id)
-                serializer = CurriculumClassListSerializer(data, many=True)
-                class_names = [item['select_class'] for item in serializer.data]
-                response_data = create_response_data(
-                    status=status.HTTP_200_OK,
-                    message=CurriculumMessage.CLASSES_LIST_MESSAGE,
-                    data=class_names
-                )
-                return Response(response_data, status=status.HTTP_200_OK)
-            else:
-                response_data = create_response_data(
-                    status=status.HTTP_400_BAD_REQUEST,
-                    message="you are not an teacher user.",
-                    data={}
-                )
-                return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+            curriculum = request.query_params.get("curriculum")
+            data = Curriculum.objects.filter(curriculum_name=curriculum, school_id=request.user.school_id)
+            serializer = CurriculumClassListSerializer(data, many=True)
+            class_names = [item['select_class'] for item in serializer.data]
+            response_data = create_response_data(
+                status=status.HTTP_200_OK,
+                message=CurriculumMessage.CLASSES_LIST_MESSAGE,
+                data=class_names
+            )
+            return Response(response_data, status=status.HTTP_200_OK)
+
         except Exception as e:
             response_data = create_response_data(
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -883,38 +860,30 @@ class TeacherCurriculumSubjectListView(APIView):
     """
     This class is created to fetch the list of subject in the provided curriculum and class.
     """
-    permission_classes = [IsTeacherUser, IsInSameSchool]
+    permission_classes = [permissions.IsAuthenticated, IsInSameSchool]
 
     def get(self, request):
         try:
-            user = request.user
-            if user.user_type == 'teacher':
-                curriculum = request.query_params.get("curriculum")
-                classes = request.query_params.get("class_name")
-                subjects = Curriculum.objects.get(school_id=request.user.school_id, curriculum_name=curriculum,
-                                                     select_class=classes)
-                subject = Subjects.objects.filter(curriculum_id=subjects)
-                serializer = CurriculumSubjectsListerializer(subject, many=True)
-                primary_subject = [item['primary_subject'] for item in serializer.data]
-                optional_subject = [item['optional_subject'] for item in serializer.data]
-                subject_list = primary_subject+optional_subject
-                # flat_subjects = [subject for sublist in subject_list for subject in sublist]
-                data = {
-                    "subject": subject_list,
-                }
-                response_data = create_response_data(
-                    status=status.HTTP_200_OK,
-                    message=CurriculumMessage.SUBJECT_LIST_MESSAGE,
-                    data=data
-                )
-                return Response(response_data, status=status.HTTP_200_OK)
-            else:
-                response_data = create_response_data(
-                    status=status.HTTP_400_BAD_REQUEST,
-                    message="you are not an teacher user.",
-                    data={}
-                )
-                return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+            curriculum = request.query_params.get("curriculum")
+            classes = request.query_params.get("class_name")
+            subjects = Curriculum.objects.get(school_id=request.user.school_id, curriculum_name=curriculum,
+                                                 select_class=classes)
+            subject = Subjects.objects.filter(curriculum_id=subjects)
+            serializer = CurriculumSubjectsListerializer(subject, many=True)
+            primary_subject = [item['primary_subject'] for item in serializer.data]
+            optional_subject = [item['optional_subject'] for item in serializer.data]
+            subject_list = primary_subject+optional_subject
+            # flat_subjects = [subject for sublist in subject_list for subject in sublist]
+            data = {
+                "subject": subject_list,
+            }
+            response_data = create_response_data(
+                status=status.HTTP_200_OK,
+                message=CurriculumMessage.SUBJECT_LIST_MESSAGE,
+                data=data
+            )
+            return Response(response_data, status=status.HTTP_200_OK)
+
         except Exception as e:
             response_data = create_response_data(
                 status=status.HTTP_400_BAD_REQUEST,
